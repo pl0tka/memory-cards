ccabf5cd45e2449054a85dd5282f98381a7b9e7e
plotva <huseva.a@gmail.com>
2024-06-12 22:51:06 +0200

    Add cancellation logic to useFetch to prevent state updates after unmount

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,29 +1,43 @@
 import { useState, useEffect } from 'react';
 
-function useFetch() {
+function useFetch(url) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    let isCanceled = false;
+
     const fetchData = async () => {
+      setLoading(true);
+
       try {
-        const resp = await fetch('https://www.moogleapi.com/api/v1/characters');
+        const resp = await fetch(url);
         if (!resp.ok) {
           throw new Error(`HTTP error: Status ${resp.status}`);
         }
         let fetchedData = await resp.json();
-        setData(fetchedData.slice(0, 16));
-        setError(null);
+        if (!isCanceled) {
+          setData(fetchedData.slice(0, 16));
+          setError(null);
+        }
       } catch (err) {
-        setError(err.message);
-        setData(null);
+        if (!isCanceled) {
+          setError(err.message);
+          setData(null);
+        }
       } finally {
-        setLoading(false);
+        if (!isCanceled) {
+          setLoading(false);
+        }
       }
     };
     fetchData();
-  }, []);
+
+    return () => {
+      isCanceled = true;
+    };
+  }, [url]);
 
   return [data, loading, error];
 }
